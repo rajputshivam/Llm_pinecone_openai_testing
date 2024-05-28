9b2efe1d42c3745060046105f528b4a8c64be4fa
rajputshivam <kumarshivam862@gmail.com>
2024-05-28 21:05:05 +0530

    updated

---
+++ ingestion.py
@@ -0,0 +1,36 @@
+import os
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Pinecone as PineconeLangChain
+from pinecone import Pinecone
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+
+
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest")
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents)}documents")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100,
+                                                   separators=["\n\n", "\n", " ", ""])
+    documents = text_splitter.split_documents(documents=raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
+
+    for doc in documents:
+        old_path = doc.metadata["source"]
+        new_url = old_path.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to insert {len(documents)} to Pinecone")
+
+    embeddings = OpenAIEmbeddings()
+    PineconeLangChain.from_documents(documents=documents, embedding=embeddings, index_name="testing")
+    print("-------------Added vector to Pinecone---------------")
+
+
+if __name__ == '__main__':
+    ingest_docs()
